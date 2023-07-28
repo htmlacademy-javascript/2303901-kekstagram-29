57936db31fba8e4f512136ea14073dffc44545e3
Artemka62 <123449625+Artemka62@users.noreply.github.com>
2023-07-28 15:11:23 +0400

    Исправил последнюю ошибку автотестов

--- js/photo-filters.js
+++ js/photo-filters.js
@@ -3,6 +3,7 @@ import {getRandomInteger, debounce} from './util.js';
 import {paintAllPictures} from './thumbnail-rendering.js';
 
 const TIME_OUT_DELAY = 500;
+const VALUE_RANDOM_PHOTO = 10;
 const buttonFilterDefauit = document.querySelector('#filter-default');
 const buttonFilterRandom = document.querySelector('#filter-random');
 const buttonFilterGiscussed = document.querySelector('#filter-discussed');
@@ -23,12 +24,20 @@ const filterDefault = (photos) => {
 
 
 const onAddFilterDefault = (photos) => {
+
   const clear = document.querySelectorAll('.picture');
+
   clear.forEach((element) => {
     element.remove();
   });
 
   filterDefault(photos);
+
+};
+
+
+const onButtonActiveDefault = () =>{
+
   buttonFilterDefauit.classList.add('img-filters__button--active');
   buttonFilterRandom.classList.remove('img-filters__button--active');
   buttonFilterGiscussed.classList.remove('img-filters__button--active');
@@ -36,7 +45,9 @@ const onAddFilterDefault = (photos) => {
 
 
 const filterRandom = (array, count) => {
+
   const clear = document.querySelectorAll('.picture');
+
   clear.forEach((element) => {
     element.remove();
   });
@@ -46,6 +57,7 @@ const filterRandom = (array, count) => {
   count = Math.min(count, array.length);
 
   while (selectedElements.size < count) {
+
     const randomIndex = getRandomInteger(0, array.length - 1);
     selectedElements.add(array[randomIndex]);
   }
@@ -57,33 +69,40 @@ const filterRandom = (array, count) => {
 const onAddFilterRandom = (photos) => {
 
   const clear = document.querySelectorAll('.picture');
+
   clear.forEach((element) => {
     element.remove();
   });
 
-  const selectedPhotos = filterRandom(photos, 10);
+  const selectedPhotos = filterRandom(photos,VALUE_RANDOM_PHOTO);
 
   paintAllPictures(selectedPhotos);
+
+};
+
+
+const onButtonActiveRandom = () =>{
+
   buttonFilterRandom.classList.add('img-filters__button--active');
   buttonFilterDefauit.classList.remove('img-filters__button--active');
   buttonFilterGiscussed.classList.remove('img-filters__button--active');
 };
 
+
 const filterGiscussed = (photos) => {
 
   const clear = document.querySelectorAll('.picture');
+
   clear.forEach((element) => {
     element.remove();
   });
 
   const sortedPictures = [...photos].slice().sort((a, b) => b.comments.length - a.comments.length);
+
   paintAllPictures(sortedPictures);
 
-  buttonFilterGiscussed.classList.add('img-filters__button--active');
-  buttonFilterDefauit.classList.remove('img-filters__button--active');
-  buttonFilterRandom.classList.remove('img-filters__button--active');
-
 };
+
 
 const onAddFilterGiscussed = (photos) => {
 
@@ -96,12 +115,24 @@ const onAddFilterGiscussed = (photos) => {
   filterGiscussed(photos);
 };
 
+
+const onButtonActiveDiscussed = () => {
+
+  buttonFilterGiscussed.classList.add('img-filters__button--active');
+  buttonFilterDefauit.classList.remove('img-filters__button--active');
+  buttonFilterRandom.classList.remove('img-filters__button--active');
+};
+
+
 const addSortToPhotos = (photos) => {
-  //paintAllPictures(photos);
+
   buttonFilterDefauit.addEventListener('click', debounce(() => onAddFilterDefault(photos), TIME_OUT_DELAY));
   buttonFilterRandom.addEventListener('click', debounce(() => onAddFilterRandom(photos), TIME_OUT_DELAY));
   buttonFilterGiscussed.addEventListener('click', debounce(() => onAddFilterGiscussed(photos), TIME_OUT_DELAY));
 };
 
+buttonFilterDefauit.addEventListener('click', onButtonActiveDefault);
+buttonFilterRandom.addEventListener('click', onButtonActiveRandom);
+buttonFilterGiscussed.addEventListener('click', onButtonActiveDiscussed);
 
 export {addSortToPhotos};
